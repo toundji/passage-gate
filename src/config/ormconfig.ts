c62941df50163ff2780c07a7fc1f638a80890792
toundji <olatoundjiagbahoungba@gmail.com>
2022-06-20 15:34:09 +0100

    Configuration du .env

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,23 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
-
+import 'dotenv/config';
 
 const ormConfig: MysqlConnectionOptions = {
     type: 'mysql',
     // url: process.env.DB_URL,
     // ssl: { rejectUnauthorized: false },
-    // host: process.env.DATABASE_HOST,
-    // port: (process.env.DATABASE_PORT as any) as number,
-    // username: process.env.DATABASE_USER,
-    // password: process.env.DATABASE_PASSWORD,
-    // database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: (process.env.DATABASE_PORT as any) as number,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "gate24_db",
+
 
     entities: ["dist/**/*.entity{.ts,.js}"],
 
